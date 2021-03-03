dd324a03c2a684b35aa6636633844f6df3f517be
tithi001 <37136040+tithi001@users.noreply.github.com>
2021-03-03 15:29:57 +0600

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,8 +1,6 @@
 (function () {
-
   names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
-
     var firstLetter = names[i].charAt(0).toLowerCase();
     if (firstLetter === 'j') {
       byeSpeaker.speak(names[i]);
